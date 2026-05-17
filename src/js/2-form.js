0ed572b64c840f313e47825b0c367131a8981b5d
Maks <pochtalion777@gmail.com>
2026-05-17 20:15:08 +0200

    did hw-9 task-2

---
+++ src/js/2-form.js
@@ -0,0 +1,40 @@
+
+function getFromLs(key) {
+    const savedData = localStorage.getItem(key);
+    if (!savedData) {
+        return null;
+    };
+    return JSON.parse(savedData);
+};
+
+const localStorageKey = 'feedback-form-state';
+const formData = getFromLs(localStorageKey) || { email: '', message: '' };
+
+const formElem = document.querySelector('.feedback-form');
+console.log(formElem);
+
+formElem.elements.email.value = formData.email;
+formElem.elements.message.value = formData.message;
+
+formElem.addEventListener('input', (e) => {
+    if (e.target.name === 'email') 
+        formData.email = e.target.value.trim();
+     else if (e.target.name === 'message') 
+        formData.message = e.target.value.trim();
+    
+    console.log(formData);
+    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+
+formElem.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (formData.email === '' || formData.message === '') {
+        return alert('Fill please all fields')
+    };
+    console.log(formData.email);
+    console.log(formData.message);
+    localStorage.removeItem(localStorageKey)
+    formElem.reset();
+});
+
